5d82eae9ee8a04c7d6e153b9b40f63a2d301abf7
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-02 17:00:33 -0500

    added bookmark by id endpoint

--- src/bookmarks/bookmarks.js
+++ src/bookmarks/bookmarks.js
@@ -11,5 +11,20 @@ bookmarksRouter
    res
       .json(bookmarks)
 })
+bookmarksRouter
+   .route('/bookmarks/:id')
+   .get((req,res)=> {
+      const {id} = req.params
+      const bookmark = bookmarks.find(b => b.id === id)
+      if (!bookmark) {
+         logger.error(`bookmark with id ${id} not found`)
+         return res
+            .status(404)
+            .send('bookmark not found')
+      }
+      res.json(bookmark)
 
+      res
+         .json(bookmark)
+   })
 module.exports = bookmarksRouter;
